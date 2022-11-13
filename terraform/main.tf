9ac54a02bd8ec69e1f9870207cbd7edd41a30f92
peperoncino <2wua4nlyi@gmail.com>
2022-11-13 13:31:36 +0900

    create cloud run using terraform

---
+++ terraform/main.tf
@@ -0,0 +1,36 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "4.43.0"
+    }
+  }
+}
+
+provider "google" {
+  project = "cloud-run-playground-368419"
+  region  = "us-central1"
+  zone    = "us-central1-c"
+
+}
+
+resource "google_cloud_run_service" "default" {
+  name     = "cloudrun-srv"
+  location = "us-central1"
+
+  template {
+    spec {
+      containers {
+        image = "us-docker.pkg.dev/cloudrun/container/hello"
+      }
+    }
+  }
+
+  traffic {
+    percent         = 100
+    latest_revision = true
+  }
+
+}
+
+
