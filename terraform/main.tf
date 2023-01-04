4af71a1f2493d2b8d63ebd0d07e1c2718b02fbb9
peperoncino <2wua4nlyi@gmail.com>
2023-01-04 16:49:29 +0900

    create ar repo

--- terraform/main.tf
+++ terraform/main.tf
@@ -78,3 +78,9 @@ resource "google_cloud_run_service" "default" {
   }
 
 }
+
+resource "google_artifact_registry_repository" "default" {
+  location = "asia-northeast1"
+  repository_id = "playground"
+  format = "DOCKER"
+}
