9e41e142126915ac23ccef5a2f81b0712e366f69
peperoncino <2wua4nlyi@gmail.com>
2023-03-03 12:47:13 +0900

    rename output val

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,7 +1,7 @@
-output "google_workload_identity_provider" {
-  value = google_iam_workload_identity_pool_provider.github-actions.name
+output "gh_actions_google_workload_identity_provider" {
+  value = google_iam_workload_identity_pool_provider.github_actions.name
 }
 
-output "google_service_account" {
-  value = google_service_account.github-actions.email
+output "gh_actions_google_service_account" {
+  value = google_service_account.github_actions.email
 }
