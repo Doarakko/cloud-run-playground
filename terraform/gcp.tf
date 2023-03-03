1766a03d3287df7a537a972783be19315dee1d53
peperoncino <2wua4nlyi@gmail.com>
2023-03-03 14:59:07 +0900

    run on terraform cloud

---
+++ terraform/gcp.tf
@@ -0,0 +1,132 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "4.53.1"
+    }
+  }
+
+  cloud {
+    organization = "Doarakko"
+
+    workspaces {
+      name = "cloud-run-playground"
+    }
+  }
+}
+
+provider "google" {
+  project = var.gcp_project_id
+  region  = var.gcp_region
+  zone    = "asia-northeast1-a"
+}
+
+provider "google-beta" {
+  project = var.gcp_project_id
+  region  = var.gcp_region
+}
+
+data "google_project" "project" {
+}
+
+resource "google_project_service" "project" {
+  project = var.gcp_project_id
+  for_each = toset([
+    "iamcredentials.googleapis.com",
+    "artifactregistry.googleapis.com",
+    "run.googleapis.com",
+    "secretmanager.googleapis.com",
+    "iam.googleapis.com",
+    "cloudresourcemanager.googleapis.com",
+    "sts.googleapis.com"
+  ])
+  service = each.value
+}
+
+resource "google_iam_workload_identity_pool" "tfc_pool" {
+  provider                  = google-beta
+  workload_identity_pool_id = "my-tfc-pool"
+}
+
+resource "google_iam_workload_identity_pool_provider" "tfc_provider" {
+  provider                           = google-beta
+  workload_identity_pool_id          = google_iam_workload_identity_pool.tfc_pool.workload_identity_pool_id
+  workload_identity_pool_provider_id = "my-tfc-provider-id"
+  attribute_mapping = {
+    "google.subject"                        = "assertion.sub",
+    "attribute.aud"                         = "assertion.aud",
+    "attribute.terraform_run_phase"         = "assertion.terraform_run_phase",
+    "attribute.terraform_project_id"        = "assertion.terraform_project_id",
+    "attribute.terraform_project_name"      = "assertion.terraform_project_name",
+    "attribute.terraform_workspace_id"      = "assertion.terraform_workspace_id",
+    "attribute.terraform_workspace_name"    = "assertion.terraform_workspace_name",
+    "attribute.terraform_organization_id"   = "assertion.terraform_organization_id",
+    "attribute.terraform_organization_name" = "assertion.terraform_organization_name",
+    "attribute.terraform_run_id"            = "assertion.terraform_run_id",
+    "attribute.terraform_full_workspace"    = "assertion.terraform_full_workspace",
+  }
+  oidc {
+    issuer_uri = "https://${var.tfc_hostname}"
+  }
+  attribute_condition = "assertion.sub.startsWith(\"organization:${var.tfc_organization_name}:project:${var.tfc_project_name}:workspace:${var.tfc_workspace_name}\")"
+}
+
+resource "google_service_account" "tfc_service_account" {
+  account_id   = "tfc-service-account"
+  display_name = "Terraform Cloud Service Account"
+}
+
+resource "google_service_account_iam_member" "tfc_service_account_member" {
+  service_account_id = google_service_account.tfc_service_account.name
+  role               = "roles/iam.workloadIdentityUser"
+  member             = "principalSet://iam.googleapis.com/${google_iam_workload_identity_pool.tfc_pool.name}/*"
+}
+
+resource "google_project_iam_member" "tfc_project_member" {
+  project = var.gcp_project_id
+  role    = "roles/editor"
+  member  = "serviceAccount:${google_service_account.tfc_service_account.email}"
+}
+
+resource "google_service_account" "github_actions" {
+  project      = var.gcp_project_id
+  account_id   = "github-actions"
+  display_name = "A service account for GitHub Actions"
+}
+
+resource "google_iam_workload_identity_pool" "github_actions" {
+  provider                  = google-beta
+  project                   = var.gcp_project_id
+  workload_identity_pool_id = "gh-oidc-pool"
+  display_name              = "gh-oidc-pool"
+  description               = "Workload Identity Pool for GitHub Actions"
+}
+
+resource "google_iam_workload_identity_pool_provider" "github_actions" {
+  provider                           = google-beta
+  project                            = var.gcp_project_id
+  workload_identity_pool_id          = google_iam_workload_identity_pool.github_actions.workload_identity_pool_id
+  workload_identity_pool_provider_id = "gh-oidc-provider"
+  display_name                       = "gh-oidc-provider"
+  description                        = "OIDC identity pool provider for GitHub Actions"
+  attribute_mapping = {
+    "google.subject"       = "assertion.sub"
+    "attribute.repository" = "assertion.repository"
+  }
+  oidc {
+    issuer_uri = "https://token.actions.githubusercontent.com"
+  }
+}
+
+resource "google_service_account_iam_member" "admin_account_iam" {
+  service_account_id = google_service_account.github_actions.name
+  role               = "roles/iam.workloadIdentityUser"
+  member             = "principalSet://iam.googleapis.com/${google_iam_workload_identity_pool.github_actions.name}/attribute.repository/${var.gh_repo_name}"
+}
+
+resource "google_project_iam_member" "admin_account_iam" {
+  project  = var.gcp_project_id
+  for_each = toset(["roles/iam.serviceAccountUser", "roles/artifactregistry.admin", "roles/run.developer"])
+  role     = each.value
+  member   = "serviceAccount:${google_service_account.github_actions.email}"
+}
---
+++ terraform/terraform.tfvars
@@ -0,0 +1,5 @@
+tfc_organization_name = "Doarakko"
+tfc_workspace_name    = "cloud-run-playground"
+gcp_project_id        = "cloud-run-playground-379503"
+gcp_region            = "asia-northeast1"
+gh_repo_name          = "Doarakko/cloud-run-playground"
